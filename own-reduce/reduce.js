4e32f55c6cda4c11e50ea38e0c2278338c1de175
lukw34 <lukw34@gmail.com>
2023-02-26 13:21:07 +0100

    semicolons

--- own-reduce/reduce.js
+++ own-reduce/reduce.js
@@ -18,17 +18,17 @@ Array.prototype.ownReduce = function(callbackFun, initialValue) {
     }
 
     return result;
-}
+};
 
 
 const sumMethod = (prev, current) => prev + current;
 
 const testArray = [1,4,5,6,7,8,9];
 
-console.log('original-reduce', testArray.reduce(sumMethod))
+console.log('original-reduce', testArray.reduce(sumMethod));
 
-console.log('own-reduce', testArray.ownReduce(sumMethod))
+console.log('own-reduce', testArray.ownReduce(sumMethod));
 
-console.log('original-reduce-10', testArray.reduce(sumMethod, 10))
+console.log('original-reduce-10', testArray.reduce(sumMethod, 10));
 
-console.log('own-reduce-10', testArray.ownReduce(sumMethod, 10))
+console.log('own-reduce-10', testArray.ownReduce(sumMethod, 10));
